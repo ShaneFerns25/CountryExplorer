a2897824c401d6ae3c2699b42e139e90576f4254
Shane Fernandes <shanesf5@gmail.com>
2023-11-26 08:15:25 +0530

    Retreive country names from country codes

--- server/routes/countries.js
+++ server/routes/countries.js
@@ -3,17 +3,33 @@ import axios from "axios";
 
 const router = express.Router();
 
-const country = 'Ireland';
+const country = 'India';
 
 router.get('/', async (req,res)=>{
     try {
+        // Retreiving important info related to the country.
         const response = await axios.get(`https://restcountries.com/v3.1/name/${country}?fullText=true`);
 
         const {name: {common,official}, independent, currencies, capital, altSpellings, region, subregion, languages, latlng, landlocked, borders, area, population, timezones, continents, flags: {png,alt}, capitalInfo} = response.data[0];
 
         // console.log(common,official, independent, currencies, capital, altSpellings, region, subregion, languages, latlng, landlocked, borders, area, population, timezones, continents, png, alt, capitalInfo);
 
-        const info = {common, official, independent, currencies, capital, altSpellings, region, subregion, languages, latlng, landlocked, borders, area, population, timezones, continents, png, alt, capitalInfo};
+        let borderCountriesList=[];
+
+        if (borders){
+            const borderCountriesCodes = borders.toString();
+            
+            // Retreiving names of countries surrounding the country in question, from their country codes.
+            const response2 = await axios.get(`https://restcountries.com/v3.1/alpha?codes=${borderCountriesCodes}`);
+
+            for (const borderCountry of response2.data){
+                const {name: {common: country}} = borderCountry;
+
+                borderCountriesList.push(country)
+            };
+        }
+
+        const info = {common, official, independent, currencies, capital, altSpellings, region, subregion, languages, latlng, landlocked, borderCountriesList, area, population, timezones, continents, png, alt, capitalInfo};
 
         console.log(info);
         res.status(200).json(info);
